0faa04845430cb681c540e9ea2f02a53bbb67886
breadjoo <yoohwanjoo@nate.com>
2024-08-07 11:57:05 +0900

    setting

---
+++ src/main/java/com/bemedicarenetwork/BeMedicareNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.bemedicarenetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeMedicareNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeMedicareNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bemedicarenetwork/BeMedicareNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bemedicarenetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeMedicareNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
